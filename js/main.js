6fd0f775924e0361b3d77fe5528b4721e6c723bf
hparra07 <howard.parra95@gmail.com>
2022-02-07 15:54:29 -0300

    version 1.0

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+
+if(document.getElementById("btnModal")){
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("btnModal");
+    var span = document.getElementsByClassName("close")[0];
+    var body = document.getElementsByTagName("body")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+
+        body.style.position = "static";
+        body.style.height = "100%";
+        body.style.overflow = "hidden";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+
+        body.style.position = "inherit";
+        body.style.height = "auto";
+        body.style.overflow = "visible";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+
+            body.style.position = "inherit";
+            body.style.height = "auto";
+            body.style.overflow = "visible";
+        }
+    }
+}
+
+menuPosition();
+ 
+$(window).scroll(function() {    
+    menuPosition();
+});
+ 
+function menuPosition() {
+    var height_header = $('.header').outerHeight(true);
+    var height_menu = $('.menu').outerHeight(true);
+ 
+    if ($(window).scrollTop() >= height_header){
+        $('.menu').addClass('fixed');
+        $('.wrapper').css('margin-top', (height_menu) + 'px');
+    } else {
+        $('.menu').removeClass('fixed');
+        $('.wrapper').css('margin-top', '0');
+    }
+}
+ 
